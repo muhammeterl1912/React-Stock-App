ff9d09dfa32a4fba89d6592bd09889fcd8ec509f
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-05-07 02:33:10 +0200

    Created pages for app and added drawer icons.

--- src/components/DrawerMenuList.jsx
+++ src/components/DrawerMenuList.jsx
@@ -1,27 +1,55 @@
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import GridViewIcon from '@mui/icons-material/GridView';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import GridViewIcon from "@mui/icons-material/GridView";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import InventoryIcon from "@mui/icons-material/Inventory";
 const DrawerMenuList = () => {
-
-const icons = [{
-    iconName:<GridViewIcon />,
-    title:"Dashboard",
-    path:"/stock",
-}]
-
+  const icons = [
+    {
+      iconName: <GridViewIcon />,
+      title: "Dashboard",
+      path: "/stock",
+    },
+    {
+      title: "Purchases",
+      iconName: <ShoppingCartIcon />,
+      path: "/stock/purchases/",
+    },
+    {
+      title: "Sales",
+      iconName: <AttachMoneyIcon />,
+      path: "/stock/sales/",
+    },
+    {
+      title: "Firms",
+      iconName: <StoreIcon />,
+      path: "/stock/firms/",
+    },
+    {
+      title: "Brands",
+      iconName: <StarsIcon />,
+      path: "/stock/brands/",
+    },
+    {
+      title: "Products",
+      iconName: <InventoryIcon />,
+      path: "/stock/products/",
+    },
+  ];
 
   return (
     <div>
       <List>
         {icons?.map((icon, index) => (
-          <ListItem  disablePadding>
+          <ListItem disablePadding>
             <ListItemButton>
-              <ListItemIcon>
-                {icon.iconName}
-              </ListItemIcon>
+              <ListItemIcon>{icon.iconName}</ListItemIcon>
               <ListItemText primary={icon.title} />
             </ListItemButton>
           </ListItem>
