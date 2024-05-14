66c0aa2b7f6c6542e36bef59e074dd5a48820cd7
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-05-14 17:20:53 +0200

    Loading ,Skeleton, and noData message"s logic has been set up in Firms.jsx

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,29 +1,51 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useStockRequest from "../services/useStockRequests";
+import Grid from "@mui/material/Grid";
 import NewFirmModal from "../components/NewFirmModal";
 import FirmsAll from "../components/FirmsAll";
+import { useSelector } from "react-redux";
 import { Box } from "@mui/material";
+import {
+  CardSkeleton,
+  ErrorMessage,
+  NoDataMessage,
+} from "../components/DataFetchMessages";
+
 const Firms = () => {
-  const { getAllListStock} = useStockRequest();
+  const { getAllListStock } = useStockRequest();
   const [open, setOpen] = useState(false);
   const [selectedFirm, setSelectedFirm] = useState(null);
+  const { firms, loading, error } = useSelector((state) => state.stock);
+
   useEffect(() => {
-  
     getAllListStock("firms");
   }, []);
 
   return (
     <div>
       <Box marginBottom="10px">
-        <NewFirmModal
-          open={open}
-          setOpen={setOpen}
-          selectedFirm={selectedFirm}
-          setSelectedFirm={setSelectedFirm}
-        />
+        {!error && (
+          <NewFirmModal
+            open={open}
+            setOpen={setOpen}
+            selectedFirm={selectedFirm}
+            setSelectedFirm={setSelectedFirm}
+          />
+        )}
       </Box>
+      {loading && (
+        <CardSkeleton>
+          <FirmsAll />
+        </CardSkeleton>
+      )}
+      {error && <ErrorMessage />}
+      {!loading && !error && !firms.length && <NoDataMessage />}
 
-      <FirmsAll setOpen={setOpen} setSelectedFirm={setSelectedFirm} />
+      {!error && !loading && firms.length > 0 && (
+        <Grid container gap={2} mt={3} justifyContent={"center"}>
+          <FirmsAll setOpen={setOpen} setSelectedFirm={setSelectedFirm} />
+        </Grid>
+      )}
     </div>
   );
 };
