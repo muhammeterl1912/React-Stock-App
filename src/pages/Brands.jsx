2b5820a8fe9d8eac8dd83142178470f2128caf77
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-05-14 17:33:31 +0200

    Brands loading,error,NoData logic also has been coded.

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,31 +1,49 @@
-import NewBrandModal from "../components/NewBrandModal"
-import { useEffect } from "react";
+import NewBrandModal from "../components/NewBrandModal";
 import useStockRequest from "../services/useStockRequests";
+import { useSelector } from "react-redux";
+import Grid from "@mui/material/Grid";
 import BrandsAll from "../components/BrandsAll";
-import { useState } from "react";
+import {
+  CardSkeleton,
+  ErrorMessage,
+  NoDataMessage,
+} from "../components/DataFetchMessages";
+import { useState, useEffect } from "react";
 const Brands = () => {
   const [open, setOpen] = useState(false);
   const [selectedBrand, setSelectedBrand] = useState(null);
-
-const {getAllListStock} =useStockRequest()
-
-
+  const { brands, loading, error } = useSelector((state) => state.stock);
+  const { getAllListStock } = useStockRequest();
 
   useEffect(() => {
     getAllListStock("brands");
   }, []);
 
-
-
   return (
     <div>
-      <NewBrandModal   open={open}
+      {!error && (
+        <NewBrandModal
+          open={open}
           setOpen={setOpen}
           selectedBrand={selectedBrand}
-          setSelectedBrand={setSelectedBrand}/>
-      <BrandsAll setOpen={setOpen} setSelectedBrand={setSelectedBrand} />
-    </div>
-  )
-}
+          setSelectedBrand={setSelectedBrand}
+        />
+      )}
+      {loading && (
+        <CardSkeleton>
+          <BrandsAll />
+        </CardSkeleton>
+      )}
+      {error && <ErrorMessage />}
+      {!loading && !error && !brands.length && <NoDataMessage />}
 
-export default Brands
+      {!error && !loading && brands.length > 0 && (
+        <Grid container gap={2} mt={3} justifyContent={"center"}>
+          <BrandsAll setOpen={setOpen} setSelectedBrand={setSelectedBrand} />
+        </Grid>
+      )}
+    </div>
+  );
+};
+
+export default Brands;
